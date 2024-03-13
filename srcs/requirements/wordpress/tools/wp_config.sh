0e62d14631158c94b5b99fc8d4368a8acb835aba
dcandan <candandilovan@gmail.com>
2024-03-13 15:12:37 +0100

    finito

--- srcs/requirements/wordpress/tools/wp_config.sh
+++ srcs/requirements/wordpress/tools/wp_config.sh
@@ -7,3 +7,14 @@ sed -i "s/votre_utilisateur_de_bdd/$SQL_USER/g" wp-config-sample.php
 sed -i "s/votre_mdp_de_bdd/$SQL_PASSWORD/g" wp-config-sample.php
 sed -i "s/localhost/mariadb:3306/g" wp-config-sample.php
 mv wp-config-sample.php wp-config.php
+
+
+wp core install --allow-root \
+                --url=dcandan.42.fr \
+                --title=YESSAILLE \
+                --admin_user=$ADMIN_NAME \
+                --admin_password=$ADMIN_PASS \
+                --admin_email=$ADMIN_EMAIL \
+                --skip-email
+
+wp user create $USER_NAME $USER_EMAIL --user_pass=$USER_PASS --allow-root
